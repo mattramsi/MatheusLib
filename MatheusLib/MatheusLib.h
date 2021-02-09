a2347d51c8888b777d38ef1dc757de4fc2450129
Matheus <matheusramossilva999@gmail.com>
2021-02-08 22:10:29 -0300

    Initial project setup

---
+++ MatheusLib/MatheusLib.h
@@ -0,0 +1,18 @@
+//
+//  MatheusLib.h
+//  MatheusLib
+//
+//  Created by Matheus Ramos on 08/02/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MatheusLib.
+FOUNDATION_EXPORT double MatheusLibVersionNumber;
+
+//! Project version string for MatheusLib.
+FOUNDATION_EXPORT const unsigned char MatheusLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MatheusLib/PublicHeader.h>
+
+
---
+++ MatheusLib/SwiftyLab.swift
@@ -0,0 +1,22 @@
+//
+//  SwiftyLab.swift
+//  MatheusLib
+//
+//  Created by Matheus Ramos on 08/02/21.
+//
+
+import Foundation
+
+public final class SwiftyLib {
+
+    let name = "SwiftyLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
