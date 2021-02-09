70114589afcfa1e8323815512169b570634fae71
Matheus <matheusramossilva999@gmail.com>
2021-02-08 22:21:30 -0300

    Added test cases

--- MatheusLibTests/MatheusLibTests.swift
+++ MatheusLibTests/MatheusLibTests.swift
@@ -10,24 +10,14 @@ import XCTest
 
 class MatheusLibTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+    var swiftyLib: SwiftyLib!
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+     override func setUp() {
+         swiftyLib = SwiftyLib()
+     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+     func testAdd() {
+         XCTAssertEqual(swiftyLib.add(a: 1, b: 1), 2)
+     }
 
 }
